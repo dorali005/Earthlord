72b0e70be804c158f6a0f92b1e53ec4a3b0405ef
Youqing Li <liyouqing2006@sina.com>
2025-12-24 00:17:12 +0800

    Add developer signature

--- Earthlord/ContentView.swift
+++ Earthlord/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+
+            Spacer()
+                .frame(height: 50)
+
+            Text("Developed by Youqing Li")
+                .font(.headline)
+                .foregroundColor(.blue)
         }
         .padding()
     }
