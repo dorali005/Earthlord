aa8d92a3c758e4e35bc313f1e8484067a1be861a
Dora <457892330@qq.com>
2025-12-23 22:40:42 +0800

    Initial Commit

---
+++ Earthlord/EarthlordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthlordApp.swift
+//  Earthlord
+//
+//  Created by qqyl on 2025/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthlordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
